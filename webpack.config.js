f2f1cc8ebdb53fd5c89385962d89a3a9734d68b6
Sebastian Stöcker <sebastian.stoecker@uni-marburg.de>
2024-04-08 14:33:11 +0200

    add vscode launch configuration and configure source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 const webpackConfig = require('@nextcloud/webpack-vue-config')
 
+webpackConfig.devtool = 'source-map'
+
 module.exports = webpackConfig
